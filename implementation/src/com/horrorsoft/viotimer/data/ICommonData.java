61534b547121a855a383642b1d511e75a57d269f
KurlesHS <kahshs@gmail.com>
2013-10-27 20:22:25 +0400

    init

---
+++ implementation/src/com/horrorsoft/viotimer/data/ICommonData.java
@@ -0,0 +1,22 @@
+package com.horrorsoft.viotimer.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexey
+ * Date: 23.10.13
+ * Time: 23:45
+ */
+public interface ICommonData {
+    public static final int TYPE_UNKNOWN = 0;
+    public static final int TYPE_COMBOBOX = 1;
+    public static final int TYPE_RADIOBUTTON = 2;
+    public static final int TYPE_SEPARATOR = 3;
+    public static final int TYPE_NUMERIC = 4;
+    public boolean isSeparator();
+    public int getPointer();
+    public String getDescription();
+    public String getDataDescription();
+    public byte[] getBinaryData();
+    public int getCurrentValue();
+    public int getType();
+}
---
+++ implementation/src/com/horrorsoft/viotimer/data/Separator.java
@@ -0,0 +1,19 @@
+package com.horrorsoft.viotimer.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexey
+ * Date: 23.10.13
+ * Time: 23:54
+ */
+public class Separator extends CommonData {
+    @Override
+    public boolean isSeparator() {
+        return true;
+    }
+
+    @Override
+    public int getType() {
+        return ICommonData.TYPE_SEPARATOR;
+    }
+}
