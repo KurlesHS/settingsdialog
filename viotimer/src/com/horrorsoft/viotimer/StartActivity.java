075398d025e8c0ed19a54d5b835c86e1f60c2d4e
KurlesHS <kahshs@gmail.com>
2014-04-04 17:48:27 +0400

    tt

--- viotimer/src/com/horrorsoft/viotimer/StartActivity.java
+++ viotimer/src/com/horrorsoft/viotimer/StartActivity.java
@@ -46,7 +46,6 @@ public class StartActivity extends SherlockActivity {
             commonData.removeBlueToothDataListener(blueToothDataListener);
             blueToothDataListener = null;
         }
-
     }
 
     @AfterViews
